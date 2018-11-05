5a562910772db4fe3fc08aaf564e67eb5dcbce81
Thierry <Thierry@DESKTOP-F4MNMJP.home>
2018-11-05 19:34:14 +0100

    Correction du main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 
 import Rework.CreateBaseDeRegle;
 import Rework.Fait;
+import Rework.Moteur;
+import Rework.Regle;
 
 
 public class Main {
@@ -12,23 +14,22 @@ public class Main {
 	public static void main(String[] args) {
 		CreateBaseDeRegle rb = new CreateBaseDeRegle("Rules");
 		rb.generateRuleBased();
+		
+		ArrayList<Regle>baseDeRegle=rb.getRulesBase();
+		
+		ArrayList<Fait>baseDeFait= new ArrayList<Fait>();
+		
+		baseDeFait.add(new Fait("auteur","=","Boris_Vian"));
 
+		baseDeFait.add(new Fait("theme","=","vengeance"));
+		
 		Fait but = new Fait("livre","=","J_irai_cracher_sur_vos_tombes");
 		
 		
-	//	Moteur m = new Moteur(baseDeFait,baseDeRegle);
+		Moteur m = new Moteur(baseDeFait,baseDeRegle);
 		
-	//	m.chainageAvant(but);
+		m.chainageAvant(but);
 		
-		//FactsBase fb= new FactsBase();
-//		fb.addFact(new Fact("auteur","Boris_Vian"));
-//		fb.addFact(new Fact("theme","dictature"));
-//		
-//		Fact target = new Fact("auteur","George_Orwell");
-//		
-//		ForwardChaining forward= new ForwardChaining(fb, rb.getRulesBase());
-//		
-//		forward.work(target);
 //	
 		}
 }
